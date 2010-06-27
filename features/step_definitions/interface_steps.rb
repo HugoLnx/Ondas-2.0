9c5906638c888d3721664cf24f12a2a178067fce
HugoLnx <hugolnx@gmail.com>
2010-06-27 11:56:47 -0700

    flexibilizacao da regex

--- features/step_definitions/interface_steps.rb
+++ features/step_definitions/interface_steps.rb
@@ -2,14 +2,6 @@ Dado /^que o usuario iniciou o programa$/ do
   Ondas2::Aplicacao.inicia
 end
 
-Entao /^ele deve vizualizar o "([^\"]*)" "([^\"]*)"$/ do |arg1, arg2|
-  pending # express the regexp above with the code you wish you had
-end
-
-Entao /^deve vizualizar o "([^\"]*)" "([^\"]*)"$/ do |arg1, arg2|
-  pending # express the regexp above with the code you wish you had
-end
-
-Entao /^deve vizualizar a "([^\"]*)" "([^\"]*)"$/ do |arg1, arg2|
-  pending # express the regexp above with the code you wish you had
+Entao /^(|ele )deve vizualizar [oa] "([^\"]*)" "([^\"]*)"$/ do |none, tipo_comp, nome_comp|
+  pending
 end
