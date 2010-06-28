5e5d7490620d5fd4fd113543e49ae8fd87516db5
HugoLnx <hugolnx@gmail.com>
2010-06-27 19:32:28 -0700

    consertando pequenos erros nos passos

--- features/step_definitions/interface_steps.rb
+++ features/step_definitions/interface_steps.rb
@@ -5,6 +5,6 @@ end
 
 Entao /^(|ele )deve vizualizar [oa] "([^\"]*)" "([^\"]*)"$/ do |none, classe_comp_s, nome_comp|
   classe_comp = Module.const_get classe_comp_s
-  comp = Ondas2::Aplicacao.componentes.o_de_tipo_e_nome classe_comp, nome_comp
+  comp = @app.componentes.o_de_classe_e_nome classe_comp, nome_comp
   comp.should be_visible
 end
