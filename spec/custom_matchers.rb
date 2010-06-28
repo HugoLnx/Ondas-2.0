1b871a0df33352f5a0de40de1280e444dcc618cb
HugoLnx <hugolnx@gmail.com>
2010-06-27 17:32:58 -0700

    criado matcher que checa de os espacos em branco foram substituidos por underlines

--- spec/custom_matchers.rb
+++ spec/custom_matchers.rb
@@ -5,4 +5,15 @@ module CustomMatchers
 			sym_s.should be_eql sym_s.downcase
 		end
 	end
+	
+	def have_underline_when_blankspace_exists_in(reference_s)
+		simple_matcher do |target|
+			sym_s = target.to_s
+			i = 0
+			reference_s.each_char do |ref_char|
+				sym_s[i].chr.should be_eql '_' if ref_char.eql? ' '
+				i += 1
+			end
+		end
+	end
 end
