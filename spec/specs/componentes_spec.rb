be5621f6811e880f0a4002cc3f382d3ed0349742
HugoLnx <hugolnx@gmail.com>
2010-06-27 18:02:01 -0700

    testando o adicionamento de componentes a classe Componentes

--- spec/specs/componentes_spec.rb
+++ spec/specs/componentes_spec.rb
@@ -2,8 +2,18 @@ require 'spec_helper'
 
 module Ondas2
 	describe Componentes do
-		it 'deveria poder ser inicializavel' do
+		it 'deveria ser inicializavel' do
 			lambda{Componentes.new}.should_not raise_error NameError
+		end
+		
+		context 'apos ser instancializado' do
+			before do
+				@componentes = Componentes.new
+			end
+			
+			it 'deveria agrupar componentes' do
+				lambda{@componentes.adiciona 10, 'dez'}.should_not raise_error
+			end
 		end
 	end
 end
