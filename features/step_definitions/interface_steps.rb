05e17bf3b709768f0c417967e83d6e6fdffae18f
HugoLnx <hugolnx@gmail.com>
2010-06-27 18:53:33 -0700

    modificando testes substituindo as referencias de categoria para referencias de classe

--- features/step_definitions/interface_steps.rb
+++ features/step_definitions/interface_steps.rb
@@ -2,8 +2,8 @@ Dado /^que o usuario iniciou o programa$/ do
   Ondas2::Aplicacao.inicia
 end
 
-Entao /^(|ele )deve vizualizar [oa] "([^\"]*)" "([^\"]*)"$/ do |none, tipo_comp, nome_comp|
-  tipo_comp = tipo_comp.para_sym
-  comp = Ondas2::Aplicacao.componentes.o_de_tipo_e_nome tipo_comp, nome_comp
+Entao /^(|ele )deve vizualizar [oa] "([^\"]*)" "([^\"]*)"$/ do |none, classe_comp_s, nome_comp|
+  classe_comp = Module.const_get classe_comp_s
+  comp = Ondas2::Aplicacao.componentes.o_de_tipo_e_nome classe_comp, nome_comp
   comp.should be_visible
 end
