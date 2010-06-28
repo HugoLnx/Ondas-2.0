b677f2269f0f0d5e672210124a7e7d95c60686bd
HugoLnx <hugolnx@gmail.com>
2010-06-28 00:12:06 -0700

    corrigindo layout e alguns bugs

--- lib/lib/aplicacao.rb
+++ lib/lib/aplicacao.rb
@@ -9,73 +9,61 @@ module Ondas2
 		def inicia
 			@janela = self.usa :classe => JFrame,
 							   :args => ['Visualizador de Ondas']
-			@janela.set_size 500,500
+			@janela.set_size 305,273
 			@janela.default_close_operation = JFrame::EXIT_ON_CLOSE
+			@janela.content_pane.layout = nil
 			@janela.resizable = false
 			@janela.visible = true
 			
-			vboxp = Box.create_vertical_box
-			
 			lbl_des_onda = self.usa :classe => JLabel,
 									:args => ['Desenho da Onda']
-			vboxp.add lbl_des_onda
+			lbl_des_onda.set_bounds(10,10,105,14)
 			
 			pnl_des = self.usa :classe => JPanel,
 							   :nome => 'desenho'
-			pnl_des.set_size 480,200
+			pnl_des.set_bounds(10,30,275,120)
 			pnl_des.background = Color::WHITE
-			vboxp.add pnl_des
-						
-			vbox1 = Box.create_vertical_box
-			
+			pnl_des.border = BorderFactory.create_line_border(Color::BLACK)
+
 			lbl_amp = self.usa :classe => JLabel,
 							   :args => ['Amplitude']
+			lbl_amp.set_bounds(10, 170, 70, 14)
 			txtf_amp = self.usa :classe => JTextField,
 								:nome => 'amplitude'
-			vbox1.add lbl_amp
-			vbox1.add txtf_amp
+			txtf_amp.set_bounds(80, 170, 59, 20)
 			
 			lbl_lam = self.usa :classe => JLabel,
 							   :args => ['Lambda']
+			lbl_lam.set_bounds(10, 190, 70, 14)
 			txtf_lam = self.usa :classe => JTextField,
 								:nome => 'lambda'
-			vbox1.add lbl_lam
-			vbox1.add txtf_lam
+			txtf_lam.set_bounds(80, 190, 59, 20)
 			
 			lbl_vel = self.usa :classe => JLabel,
 							   :args => ['Velocidade']
+			lbl_vel.set_bounds(10, 210, 70, 14)
 			txtf_vel = self.usa :classe => JTextField,
 								:nome => 'velocidade'
-			vbox1.add lbl_vel
-			vbox1.add txtf_vel
-			
-			vbox2 = Box.create_vertical_box
+			txtf_vel.set_bounds(80, 210, 59, 20)
 			
 			lbl_freq = self.usa :classe => JLabel,
 							   :args => ['Frequencia']
+			lbl_freq.set_bounds(150, 170, 70, 14)
 			txtf_freq = self.usa :classe => JTextField,
 								:nome => 'frequencia'
-			vbox2.add lbl_freq
-			vbox2.add txtf_freq
+			txtf_freq.set_bounds(220, 170, 59, 20)
 			
 			lbl_per = self.usa :classe => JLabel,
 							   :args => ['Periodo']
+			lbl_per.set_bounds(150, 190, 70, 14)
 			txtf_per = self.usa :classe => JTextField,
 								:nome => 'periodo'
-			vbox2.add lbl_per
-			vbox2.add txtf_per
+			txtf_per.set_bounds(220, 190, 59, 20)
 			
 			btn_gerar = self.usa :classe => JButton,
 								:args => ['Gerar Onda']
 			
-			hbox = Box.create_horizontal_box
-			
-			hbox.add vbox1
-			hbox.add vbox2
-			vboxp.add hbox
-			vboxp.add btn_gerar
-			@janela.add vboxp
-					 
+			btn_gerar.set_bounds(150, 210, 128, 19)
 		end
 		
 		def usa(args={})
@@ -88,6 +76,7 @@ module Ondas2
 			nome = comp.text if nome.nil? and (classe == JButton or classe == JLabel)
 			nome = comp.title if classe == JFrame and nome.nil?
 			self.componentes.adiciona comp, nome
+			@janela.content_pane.add comp if classe != JFrame and !@janela.nil?
 			comp
 		end
 	end
--- lib/ondas2.rb
+++ lib/ondas2.rb
@@ -3,13 +3,18 @@ $LOAD_PATH << File.join(aqui,'bin')
 
 require 'java'
 
+java_import javax.swing.BorderFactory
+java_import javax.swing.Box
+java_import javax.swing.BoxLayout
 java_import javax.swing.JButton
 java_import javax.swing.JFrame
 java_import javax.swing.JLabel
 java_import javax.swing.JPanel
 java_import javax.swing.JTextField
-java_import javax.swing.Box
+
 java_import java.awt.Color
+java_import java.awt.Dimension
+
 
 Dir[File.join(aqui,'bin/**.class')].each do |class_arq|
 	class_arq = class_arq.slice(/bin\/(.*)\.class$/,1)
